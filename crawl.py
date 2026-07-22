f0506223ef9e4ee8e67ea975c1af30817f097ae9
rhys <rhysespich@gmail.com>
2026-07-21 20:10:02 -0500

    added and tested get_heading_from_html function to crawl.py

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlsplit
+from bs4 import BeautifulSoup, Tag
 
 def normalize_url(url: str) -> str:
     parts = urlsplit(url)
@@ -9,3 +10,12 @@ def normalize_url(url: str) -> str:
         path = parts.path[:-1]
     
     return f'{domain}{path}'
+
+def get_heading_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    h_tag = soup.h1
+    if isinstance(h_tag, Tag)==False:
+        h_tag = soup.h2
+    
+    return h_tag.get_text(strip=True, separator=" ") if isinstance(h_tag, Tag) else ""
