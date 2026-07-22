a66e5a928e42a6a8f869b411de6191f339016833
rhys <rhysespich@gmail.com>
2026-07-22 00:37:17 -0500

    added function to grab urls from html files. Tested with unit tests. Edited out some redundent tests from test_crawl.py

--- crawl.py
+++ crawl.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
 
 def normalize_url(url: str) -> str:
@@ -14,9 +14,7 @@ def normalize_url(url: str) -> str:
 def get_heading_from_html(html: str) -> str:
     soup = BeautifulSoup(html, 'html.parser')
 
-    h_tag = soup.h1
-    if isinstance(h_tag, Tag)==False:
-        h_tag = soup.h2
+    h_tag = soup.find("h1") or soup.find("h2")
     
     return h_tag.get_text(strip=True, separator=" ") if isinstance(h_tag, Tag) else ""
 
@@ -28,3 +26,18 @@ def get_first_paragraph_from_html(html: str) -> str:
         p_tag = soup.main.p
 
     return p_tag.get_text(strip=True, separator=" ") if isinstance(p_tag, Tag) else ""
+
+def get_urls_from_html(html: str, base_url: str):
+    soup = BeautifulSoup(html, 'html.parser')
+    abs_path_list = []
+
+    a_tag_list = soup.find_all('a')
+
+    for item in a_tag_list:
+        url = item.get('href') 
+        if url == None or url == "":
+            continue
+        abs_path_list.append(urljoin(base_url, url))
+   
+   
+    return abs_path_list
