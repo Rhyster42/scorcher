246c10d8363102349e1475f3f991ee0f3b36675b
rhys <rhysespich@gmail.com>
2026-07-22 13:09:30 -0500

    added get html funciton to make the http request for the html from a website url.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, requests
 
 def main():
     args = sys.argv
@@ -11,6 +11,20 @@ def main():
         sys.exit(1)
     else:
         print(f"starting crawl of: {args[1]}")
+        url = args[1]
+
+    print(get_html(url))
+
+def get_html(url: str) -> str:
+    response = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"})
+
+    if response.status_code >= 400:
+        raise Exception("Request failed")
+    if 'text/html' not in response.headers['Content-Type']:
+        raise Exception(f'Incorrect content type: {response.headers["Content-Type"]}')
+    if response.status_code != 200:
+        raise Exception(f'Error: {response.status_code} - {response.raise_for_status}')
+    return response.text
 
 if __name__ == "__main__":
     main()
