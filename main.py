3ddbd2d14c921add534e73c829849be95b79114a
rhys <rhysespich@gmail.com>
2026-07-22 12:29:20 -0500

    built system argument capture for main.py for the cli tool commands.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from scorcher!")
+import sys
 
+def main():
+    args = sys.argv
+
+    if len(args) < 2:
+        print("no website provided") 
+        sys.exit(1)
+    elif len(args) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    else:
+        print(f"starting crawl of: {args[1]}")
 
 if __name__ == "__main__":
     main()
