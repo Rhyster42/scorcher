9d77992e006467468bea89cb612317d740043476
rhys <rhysespich@gmail.com>
2026-07-24 06:20:58 -0500

    refactored asynccrawler class to have a max conccurency and max pages limit so it can throttle itself on larger websites. added arguments to cli tool in main.py

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ from crawl import crawl_site_async
 async def main() -> None:
     args = sys.argv
 
-    if len(args) < 2:
+    if len(args) < 4:
         print("no website provided") 
         sys.exit(1)
-    elif len(args) > 2:
+    elif len(args) > 4:
         print("too many arguments provided")
         sys.exit(1)
     else:
         print(f"Starting crawl of: {args[1]}")
         base_url = args[1]
 
-    data = await crawl_site_async(base_url)
+    if not args[2].isdigit():
+        print("max_concurrency must be an integer")
+        sys.exit(1)
+    if not args[3].isdigit():
+        print("max_pages must be an integer")
+        sys.exit(1)
+
+    max_concurrency = int(args[2])
+    max_pages = int(args[3])
+
+    print(f'Starting async crawl of: {base_url}')
+
+    data = await crawl_site_async(base_url, max_concurrency, max_pages)
 
     for page in data.values():
         print(f'Located {len(page["outgoing_links"])} outgoing links on {page["url"]}')
